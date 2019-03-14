7acf961b826d8625559a87b4bdd45970ac44167d
AD-7 <dominiak.aleksander@onet.pl>
2019-03-14 13:12:23 +0100

    prototyp próbny

---
+++ Wataha/Assets/HUDController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+
+    public Text numberOfMeat;
+    public Text numberOfWhiteFangs;
+    public Text numberOfGoldFangs;
+    public int Meat=200, WhiteFangs=100, GoldFangs=0;
+    public Button wolf1;
+    public Button wolf2;
+
+    public GameObject wolfScreen;
+    private Text wolfScreenTitle;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        wolf1.onClick.AddListener(Wolf1Clicked);
+        wolf2.onClick.AddListener(Wolf2Clicked);
+        numberOfMeat.text = Meat.ToString();
+        numberOfWhiteFangs.text = WhiteFangs.ToString();
+        numberOfGoldFangs.text = GoldFangs.ToString();
+        wolfScreen.gameObject.SetActive(false);
+        wolfScreenTitle = wolfScreen.GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        numberOfMeat.text = Meat.ToString();
+        numberOfWhiteFangs.text = WhiteFangs.ToString();
+        numberOfGoldFangs.text = GoldFangs.ToString();
+    }
+    
+    void Wolf1Clicked()
+    {
+        wolfScreenTitle.text = "Wolf1";
+        wolfScreen.gameObject.SetActive(true);
+        
+    }
+    void Wolf2Clicked()
+    {
+        wolfScreenTitle.text = "Wolf2";
+        wolfScreen.gameObject.SetActive(true);
+
+    }
+}
---
+++ Wataha/Assets/Moving.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public int speed = 5;
+    private Transform position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.transform.Translate(new Vector3(0, 0, 0.05f));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+           position.transform.Translate(new Vector3(0.01f, 0.0f, 0.0f));
+            position.Rotate(new Vector3(0, 1, 0), 0.5f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.transform.Translate(new Vector3(-0.01f, 0.0f, 0.0f));
+            position.Rotate(new Vector3(0, -1, 0), 0.5f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            position.transform.Translate(new Vector3(0f, 0.0f, -0.02f));
+           
+        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    Vector3 oldPos = position.position;
+        //    GetComponent<Rigidbody>().AddForce(new Vector3(0, 50f, 0));
+        //}
+    }
+}
