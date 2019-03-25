449bb93209bb3759898d8e772bd55b1d9c27a244
AD-7 <dominiak.aleksander@onet.pl>
2019-03-25 21:50:12 +0100

    drwal

---
+++ Wataha/Assets/Quest/CollectBigMushrooms.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectBigMushrooms : Quest
+{
+    public Text buttonInfo;
+    private GameObject wataha;
+    private int mushroomsLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonInfo.gameObject.SetActive(false);
+        wataha = GameObject.FindGameObjectWithTag("Player");
+        mushroomsLeft = this.questItems.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IfInRatio())
+        {
+            buttonInfo.gameObject.SetActive(true);
+        }
+
+   }
+
+public bool IfInRatio()
+    {
+        for(int i =0; i<4; i++)
+        {
+         if(
+            (wataha.transform.position.x - questItems[i].transform.position.x) < 3.0f &&
+            (wataha.transform.position.x - questItems[i].transform.position.x) > -3.0f &&
+              (wataha.transform.position.x - questItems[i].transform.position.x) < 3.0f &&
+                (wataha.transform.position.x - questItems[i].transform.position.x) > -3.0f)
+            {
+                return true;
+            }
+
+            
+        }
+        return false;
+    }
+
+}
