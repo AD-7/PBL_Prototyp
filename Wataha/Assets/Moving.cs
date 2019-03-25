5587bc5c6e9ad2f72535658bf6d123dbcf4741ce
AD-7 <dominiak.aleksander@onet.pl>
2019-03-25 17:36:58 +0100

    Revert "poprawki w poruszaniu"
    This reverts commit 14b709e16de62b21dc773fba62c9322748db5554.

--- Wataha/Assets/Moving.cs
+++ Wataha/Assets/Moving.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Moving : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public int speed = 5;
     private Transform position;
-    public GameObject wolves;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,21 +15,19 @@ public class Moving : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-     
-   
         if (Input.GetKey(KeyCode.W))
         {
-            position.transform.Translate(new Vector3(0, 0, speed));
+            position.transform.Translate(new Vector3(0, 0, 0.1f));
         }
         if (Input.GetKey(KeyCode.D))
         {
            position.transform.Translate(new Vector3(0.01f, 0.0f, 0.0f));
-            position.Rotate(new Vector3(0, 1, 0), 0.4f);
+            position.Rotate(new Vector3(0, 1, 0), 0.5f);
         }
         if (Input.GetKey(KeyCode.A))
         {
             position.transform.Translate(new Vector3(-0.01f, 0.0f, 0.0f));
-            position.Rotate(new Vector3(0, -1, 0), 0.4f);
+            position.Rotate(new Vector3(0, -1, 0), 0.5f);
         }
         if (Input.GetKey(KeyCode.S))
         {
