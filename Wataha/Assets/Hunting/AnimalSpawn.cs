81abd170aeb6d8662603b237840e40663540ed28
Mantis1262 <Mateuszkarolak3@o2.pl>
2019-04-01 22:34:04 +0200

    Spawn Animal

---
+++ Wataha/Assets/Hunting/AnimalSpawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AnimalSpawn : MonoBehaviour
+{
+    public GameObject enemy;                // The enemy prefab to be spawned.
+    public float spawnTime = 3f;            // How long between each spawn.
+    public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
+    public int maxAnimal = 10;
+
+
+    void Start()
+    {
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        if (GameObject.FindGameObjectsWithTag("Animal").Length > maxAnimal)
+        {
+            return;
+        }
+
+        // Find a random index between zero and one less than the number of spawn points.
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
