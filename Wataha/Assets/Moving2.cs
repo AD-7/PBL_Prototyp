33ebbdc76b92820b7fa4d78fa788239de16814fd
AD-7 <dominiak.aleksander@onet.pl>
2019-04-01 19:05:42 +0200

    szybkość poruszania zależna od speed

---
+++ Wataha/Assets/Moving2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving2 : MonoBehaviour
+{
+    private Transform position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+     
+        if (Input.GetKey(KeyCode.W))
+        {
+           position.transform.Translate(new Vector3(0, 0, 1.0f * GetComponent<Wolf>().speed * Time.deltaTime));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+           position.transform.Translate(new Vector3(0.01f, 0.0f, 0.0f));
+            position.Rotate(new Vector3(0, 1, 0), 1.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.transform.Translate(new Vector3(-0.01f, 0.0f, 0.0f));
+            position.Rotate(new Vector3(0, -1, 0), 1.0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            position.transform.Translate(new Vector3(0f, 0.0f, -0.05f));
+           
+        }
+
+        if (Input.GetButton("Sprint"))
+        {
+            position.transform.Translate(new Vector3(0, 0, 1.5f * GetComponent<Wolf>().speed * Time.deltaTime));
+
+        }
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    Vector3 oldPos = position.position;
+        //    GetComponent<Rigidbody>().AddForce(new Vector3(0, 50f, 0));
+        //}
+    }
+}
